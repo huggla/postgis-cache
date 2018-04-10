bdfaf24bdaa438b48f5b53f2e8532efa199bf66e
huggla <henrik.uggla@kristianstad.se>
2018-04-10 13:24:09 +0200

    Update 100.cache.sh

--- initdb/100.cache.sh
+++ initdb/100.cache.sh
@@ -66,11 +66,11 @@ do
    echo "ALTER DEFAULT PRIVILEGES IN SCHEMA \"$fschema\" GRANT SELECT ON TABLES TO \"$USER\";" >> "$sql_file"
    if [ -z "$foreign_server_schema_tables" ]
    then
-      foreign_server_schema_tables="$("$psql_cmd" -q -A -t -R , --dbname="$DATABASE" -c "SELECT table_name FROM information_schema.tables WHERE table_schema='$ftable_schema'")"
+      foreign_server_schema_tables="$($psql_cmd -q -A -t -R , --dbname="$DATABASE" -c "SELECT table_name FROM information_schema.tables WHERE table_schema='$ftable_schema'")"
    fi   
    for ftable in $foreign_server_schema_tables
    do
-      "$psql_cmd" --dbname="$DATABASE" -c "CREATE MATERIALIZED VIEW $fschema.$ftable AS SELECT * FROM $ftable_schema.$ftable WITH DATA;"
+      $psql_cmd --dbname="$DATABASE" -c "CREATE MATERIALIZED VIEW $fschema.$ftable AS SELECT * FROM $ftable_schema.$ftable WITH DATA;"
    done
 done
 IFS=$IFS_tmp
